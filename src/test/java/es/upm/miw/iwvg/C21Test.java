9f92e272b0e3853f74de5cc8a52ce3b5d6461b0b
robertome <roberto.martin.escribano@alumnos.upm.es>
2018-09-18 10:06:19 +0200

    Incluido metodo m3() a C21 y metodo testM3() a C21Test

--- src/test/java/es/upm/miw/iwvg/C21Test.java
+++ src/test/java/es/upm/miw/iwvg/C21Test.java
@@ -13,6 +13,11 @@ public class C21Test {
 
     @Test
     public void testM2() {
-        assertEquals("m1", new C21().m1());
+        assertEquals("m2", new C21().m2());
+    }
+
+    @Test
+    public void testM3() {
+        assertEquals("m3", new C21().m3());
     }
 }
