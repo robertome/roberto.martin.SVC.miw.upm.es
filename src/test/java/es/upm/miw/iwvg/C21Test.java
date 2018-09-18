eb46d27cc4c04efc0b36030c9599ebe34307d23f
robertome <roberto.martin.escribano@alumnos.upm.es>
2018-09-18 09:10:42 +0200

    Creada clase C21Test

---
+++ src/test/java/es/upm/miw/iwvg/C21Test.java
@@ -0,0 +1,18 @@
+package es.upm.miw.iwvg;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class C21Test {
+
+    @Test
+    public void testM1() {
+        assertEquals("m1", new C21().m1());
+    }
+
+    @Test
+    public void testM2() {
+        assertEquals("m1", new C21().m1());
+    }
+}
